fe496a0fed3227d72f804ae0b77182d1ec0dcd10
25487 <43673192+25487@users.noreply.github.com>
2021-01-23 22:57:34 +0100

    update aan email verification ect

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,10 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Auth::routes();
 Route::post('/auth','AdminController@index');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('verified');
 Route::get('/user', 'UserController@index')->name('user');
 Route::get('/admin', 'AdminController@index')->name('admin');
+//Route::get('/users', [UserController::class, 'index']);
+//Route::get('/user', 'UserController@index')->name('user');
+//Route::get("users", 'UserController@index');
+
+Route::get("users", [Users::class,'index']);
